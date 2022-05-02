776e417c491f9cc10eaf82cd88d6796d8e86e5fb
masukoreo <0317masuo@gmail.com>
2022-05-02 17:55:31 +0900

    js課題

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,10 @@
 document.addEventListener('submit',e=>{
     const req=[...e.target.querySelectorAll('[data-required]')];
-    const empty=req.filter(x=>x.value=="");
-    const list={"name":"お名前","email":"メールアドレス","tel":"電話番号","message":"お問い合わせ内容"};
+    const empty=req.filter(x=>x.value=='');
+    const list={'name':'お名前','email':'メールアドレス','tel':'電話番号','message':'お問い合わせ内容'};
     if(req && empty){
       empty[0].focus();
       alert(list[empty[0].name]+'を入力してください');
       e.preventDefault();
     }
-  });
+  });  
