f2d0a4132ed88c62d70bd2a32c35c57b6a80e589
masukoreo <0317masuo@gmail.com>
2022-05-26 00:00:12 +0900

    無

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,21 @@
-document.addEventListener('submit',e=>{
-    const req=[...e.target.querySelectorAll('[data-required]')];
-    const empty=req.filter(x=>x.value=='');
-    const list={'name':'お名前','email':'メールアドレス','tel':'電話番号','message':'お問い合わせ内容'};
-    if(req && empty){
-      empty[0].focus();
-      alert(list[empty[0].name]+'を入力してください');
-      e.preventDefault();
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener('click',() => {
+    const text = document.forms.textBox.name.value;
+    const text2 = document.forms.textBox.email.value;
+    const text3 = document.forms.textBox.tel.value;
+    const text4 = document.forms.textBox.message.value;
+
+    if(text=="") {
+        alert("名前を入力してください");
+      }
+    else if (text2=="") {
+        alert("メールアドレスを入力してください")
     }
-  });  
+    else if (text3=="") {
+        alert("電話番号を入力してください")
+    }
+    else if(text4=="") {
+        alert("お問い合わせ内容を入力してください")
+    }
+},false);
